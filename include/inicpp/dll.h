742643d1b444cc2e6a2df58fd9aa4f7326b177d4
Martin Polanka <PolankaMartin@gmail.com>
2016-05-12 22:10:48 +0200

    Disable warnings on MSVC++

--- include/inicpp/dll.h
+++ include/inicpp/dll.h
@@ -1,6 +1,7 @@
 #ifndef INICPP_DLL_H
 #define INICPP_DLL_H
 
+
 #ifdef INICPP_DLL
 #ifdef INICPP_EXPORT
 #define INICPP_API __declspec(dllexport)
@@ -10,5 +11,11 @@
 #else
 #define INICPP_API
 #endif
+
+
+// Disable unwanted and not necessary MSVC++ warnings
+#pragma warning(disable:4800)
+#pragma warning(disable:4251)
+
 
 #endif // INICPP_DLL_H
